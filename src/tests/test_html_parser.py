9c8e58449f94c4c2162c1a32032fc6d60ea29370
troyld <troyld@163.com>
2022-09-13 22:43:29 +0800

    reorganize files directory

--- src/tests/test_html_parser.py
+++ src/tests/test_html_parser.py
@@ -5,6 +5,8 @@ import networkx as nx
 from matplotlib import pyplot as plt
 
 from brainstorming.search.parser import html
+import tests
+import os
 
 logging.basicConfig(
     level=logging.DEBUG, format='%(asctime)s [%(name)s] %(levelname)s: %(message)s'
@@ -13,10 +15,14 @@ logging.basicConfig(
 
 class TestHtmlClustering(TestCase):
 
+    def setUp(self) -> None:
+        self.base_dir = os.path.dirname(tests.__file__)
+
     def test_google_search_html_parser(self):
 
         url = "https://www.google.com"
-        with open("resource/google-search.html") as fp:
+        path = "resource/google-search.html"
+        with open(os.path.join(self.base_dir, path)) as fp:
             nodes = html.parse_as_nodes(fp.read(), url)
         cluster = html.do_dbscan(html.nodes_as_array(nodes), eps=0.2)
         group = {}
@@ -31,7 +37,8 @@ class TestHtmlClustering(TestCase):
     def test_baidu_search_html_parser(self):
 
         url = "https://www.baidu.com"
-        with open("resource/baidu-search.html") as fp:
+        path = "resource/baidu-search.html"
+        with open(os.path.join(self.base_dir, path)) as fp:
             nodes = html.parse_as_nodes(fp.read(), url)
         cluster = html.do_dbscan(html.nodes_as_array(nodes), eps=0.2)
         group = {}
@@ -46,7 +53,8 @@ class TestHtmlClustering(TestCase):
     def test_bing_search_html_parser(self):
 
         url = "https://www.bing.com"
-        with open("resource/bing-search.html") as fp:
+        path = "resource/bing-search.html"
+        with open(os.path.join(self.base_dir, path)) as fp:
             nodes = html.parse_as_nodes(fp.read(), url)
         cluster = html.do_dbscan(html.nodes_as_array(nodes), eps=0.2)
         group = {}
